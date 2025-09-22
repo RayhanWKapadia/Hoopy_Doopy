049535f55e663b3816cc4e141aef124a3b74ba12
John Doe <johndoe@email.com>
2025-09-21 21:24:15 -0400

    Added seeding data along with exposing port 5432 to access database in local client

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+  <DataTable tableStyle="min-width: 50rem">
+    <Column field="code" header="Code"></Column>
+    <Column field="name" header="Name"></Column>
+    <Column field="category" header="Category"></Column>
+    <Column field="quantity" header="Quantity"></Column>
+  </DataTable>
+    
   </main>
 </template>
