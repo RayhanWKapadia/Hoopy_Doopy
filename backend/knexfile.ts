c48cba01bf0d760864142fa31a89abcaace0734b
rayhan-kapadia <kapadiarayhan1234@gmail.com>
2025-10-22 18:26:21 -0400

    Configuring database connection

--- backend/knexfile.ts
+++ backend/knexfile.ts
@@ -1,15 +1,22 @@
-export default {
+import dotenv from 'dotenv';
+import type { Knex } from 'knex';
+
+dotenv.config();
+
+const config: { [key: string]: Knex.Config } = {
   development: {
     client: 'pg',
     connection: {
-      host: process.env.POSTGRES_HOST || 'postgres',
-      user: process.env.POSTGRES_USER || 'postgres',
-      password: process.env.POSTGRES_PASSWORD || 'password',
-      database: process.env.POSTGRES_DB || 'app_db',
-      port: 5432,
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
     },
     migrations: {
-      directory: './migrations'
-    }
-  }
+      directory: './migrations',
+    },
+  },
 };
+
+export default config;
